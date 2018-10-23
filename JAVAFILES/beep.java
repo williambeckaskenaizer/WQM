3840842adaf77cc18907c953e39361bf82f9b785
root <root@Mother-Base.localdomain>
2018-10-23 14:42:44 -0700

    more formulas. does not compile, needs translation.

--- JAVAFILES/beep.java
+++ JAVAFILES/beep.java
@@ -7,6 +7,10 @@ public class beep{
 	float pH;
 	float alkalinity;
 	float phTarget;
+	float finalAlkalinity;
+	float ct;
+	float alpha1;
+	float alpha2
 
 	temperature = 15.0f;
 	tds = 57f;
@@ -14,18 +18,21 @@ public class beep{
 	alkalinity = 35f;
 	phTarget = 10.3f;
 
+	alpha1 = (float)Math.pow(10,(-0.0025*Math.sqrt(tds)));
+	alpha2 = (float)Math.pow(10,((-0.01*Math.sqrt(tds))));
+
 	float k1 = (float)Math.pow(10f,(-1*(356.309-21834.4/(273+temperature)-126.834*Log10(273f+temperature)+0.06092*(273+temperature)+1684915/Math.pow((273.0f+temperature),2))));
 	float k2 = (float)Math.pow(10,(-1*(107.887-5151.8/(273+temperature)-38.926*Log10(273+temperature)+0.032528*(273+temperature)+563713.9/Math.pow((273+temperature),2))));
 	float kw = (float)Math.pow(10,(-1*(-6.088+4471/(273+temperature)+0.01706*(273+temperature))));
+
+	ct = (1+alpha1*10^(-pHi)/k1 + alpha1*k2/alpha2/10^(-pHi))*((alki/50000-Kw/alpha1/10^(-pHi)+10^(-pHi)/alpha1)/(1+2*k2*alpha1/alpha2/10^(-pHi)));
+	finalAlkalinity = 50000*(CT*(1+2*k2*alpha1/alpha2/10^(-pHf))/(1+alpha1*10^(-pHf)/k1 +alpha1*k2/alpha2/10^(-pHf))+Kw/alpha1/10^(-pHf)-10^(-pHf)/alpha1);
 
 	float k1pk = -Log10(k1);
 	float k2pk = -Log10(k2);
 	float kwpk = -Log10(kw);
 
-	float alpha1 = (float)Math.pow(10,(-0.0025*Math.sqrt(tds)));
-	float alpha2 = (float)Math.pow(10,((-0.01*Math.sqrt(tds))));
-
-	//float h2so4 = sulfuricAcidCalc(phTarget, pH, );
+	float h2so4 = sulfuricAcidCalc(phTarget, pH, );
 
 	System.out.println("K1 = " + k1);
 	System.out.println("K2 = " + k2);
@@ -45,6 +52,9 @@ public class beep{
 		return (float) Math.log10(x);
 	}
 	public static float sulfuricAcidCalc(){
+		if(pH > phTarget){
+			return (49/50)*
+		}
 		return 0.0f;
 	}
 }
