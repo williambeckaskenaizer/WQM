578c0fb37d93651a733536d05190ab408fc8aeda
Will <willionaire14@gmail.com>
2019-01-24 12:20:38 -0800

    added up to c302negative

--- JAVAFILES/AcidBaseAdditionModel.java
+++ JAVAFILES/AcidBaseAdditionModel.java
@@ -1,64 +1,64 @@
-public class AcidBaseAdditionModel{
-	public static void main(String args[]){
-	System.out.println("Testing equations in java:");
+public class AcidBaseAdditionModel {
+public static void main(String args[]){
+								System.out.println("Testing equations in java:");
 
-	float temperature;
-	float tds;
-	float pH;
-	float alkalinity;
-	float phTarget;
-	float finalAlkalinity;
-	float ct;
-	float alpha1;
-	float alpha2;
+								float temperature;
+								float tds;
+								float pH;
+								float alkalinity;
+								float phTarget;
+								float finalAlkalinity;
+								float ct;
+								float alpha1;
+								float alpha2;
 
-	temperature = 10.0f;
-	tds = 57f;
-	pH = 8.5f;
-	alkalinity = 35f;
-	phTarget = 10.3f;
+								temperature = 10.0f;
+								tds = 57f;
+								pH = 8.5f;
+								alkalinity = 35f;
+								phTarget = 10.3f;
 
-	alpha1 = (float)Math.pow(10,(-0.0025*Math.sqrt(tds)));
-	alpha2 = (float)Math.pow(10,((-0.01*Math.sqrt(tds))));
+								alpha1 = (float)Math.pow(10,(-0.0025*Math.sqrt(tds)));
+								alpha2 = (float)Math.pow(10,((-0.01*Math.sqrt(tds))));
 
-	float k1 = (float)Math.pow(10f,(-1*(356.309-21834.4/(273+temperature)-126.834*Log10(273f+temperature)+0.06092*(273+temperature)+1684915/Math.pow((273.0f+temperature),2))));
-	float k2 = (float)Math.pow(10,(-1*(107.887-5151.8/(273+temperature)-38.926*Log10(273+temperature)+0.032528*(273+temperature)+563713.9/Math.pow((273+temperature),2))));
-	float kw = (float)Math.pow(10,(-1*(-6.088+4471/(273+temperature)+0.01706*(273+temperature))));
+								float k1 = (float)Math.pow(10f,(-1*(356.309-21834.4/(273+temperature)-126.834*Log10(273f+temperature)+0.06092*(273+temperature)+1684915/Math.pow((273.0f+temperature),2))));
+								float k2 = (float)Math.pow(10,(-1*(107.887-5151.8/(273+temperature)-38.926*Log10(273+temperature)+0.032528*(273+temperature)+563713.9/Math.pow((273+temperature),2))));
+								float kw = (float)Math.pow(10,(-1*(-6.088+4471/(273+temperature)+0.01706*(273+temperature))));
 
-	ct = (float)((1+alpha1*Math.pow(10,(-pH))/k1 + alpha1*k2/alpha2/Math.pow(10, (-pH)))*((alkalinity/50000-kw/alpha1/Math.pow(10,(-pH))+Math.pow(10, (-pH))/alpha1)/(1+2*k2*alpha1/alpha2/Math.pow(10,(-pH)))));
-	finalAlkalinity = (float)(50000*(ct*(1+2*k2*alpha1/alpha2/Math.pow(10,(-phTarget)))/(1+alpha1*Math.pow(10,(-phTarget))/k1 +alpha1*k2/alpha2/Math.pow(10,(-phTarget)))+kw/alpha1/Math.pow(10,(-phTarget))-Math.pow(10,(-phTarget))/alpha1));
+								ct = (float)((1+alpha1*Math.pow(10,(-pH))/k1 + alpha1*k2/alpha2/Math.pow(10, (-pH)))*((alkalinity/50000-kw/alpha1/Math.pow(10,(-pH))+Math.pow(10, (-pH))/alpha1)/(1+2*k2*alpha1/alpha2/Math.pow(10,(-pH)))));
+								finalAlkalinity = (float)(50000*(ct*(1+2*k2*alpha1/alpha2/Math.pow(10,(-phTarget)))/(1+alpha1*Math.pow(10,(-phTarget))/k1 +alpha1*k2/alpha2/Math.pow(10,(-phTarget)))+kw/alpha1/Math.pow(10,(-phTarget))-Math.pow(10,(-phTarget))/alpha1));
 
-	float k1pk = -Log10(k1);
-	float k2pk = -Log10(k2);
-	float kwpk = -Log10(kw);
+								float k1pk = -Log10(k1);
+								float k2pk = -Log10(k2);
+								float kwpk = -Log10(kw);
 
-	float h2so4 = sulfuricAcidCalc(pH, phTarget, alkalinity, finalAlkalinity);
+								float h2so4 = sulfuricAcidCalc(pH, phTarget, alkalinity, finalAlkalinity);
 
-	System.out.println("K1 = " + k1);
-	System.out.println("K2 = " + k2);
-	System.out.println("Kw = " + kw);
+								System.out.println("K1 = " + k1);
+								System.out.println("K2 = " + k2);
+								System.out.println("Kw = " + kw);
 
-	System.out.println("k1 pK = " + k1pk);
-	System.out.println("k2 pK = " + k2pk);
-	System.out.println("kw pK = " + kwpk);
+								System.out.println("k1 pK = " + k1pk);
+								System.out.println("k2 pK = " + k2pk);
+								System.out.println("kw pK = " + kwpk);
 
-	System.out.println("Alpha1 = " + alpha1);
-	System.out.println("Alpha2 = " + alpha2);
+								System.out.println("Alpha1 = " + alpha1);
+								System.out.println("Alpha2 = " + alpha2);
 
-	System.out.println("Final Alkalinity = " + Math.round(finalAlkalinity));
-	System.out.println("CT = " + ct);
+								System.out.println("Final Alkalinity = " + Math.round(finalAlkalinity));
+								System.out.println("CT = " + ct);
 
-	System.out.println("H2SO4 = " + h2so4);
-	}
+								System.out.println("H2SO4 = " + h2so4);
+}
 
-	public static float Log10(float x){
-		return (float) Math.log10(x);
-	}
-	public static float sulfuricAcidCalc(float pH, float phTarget, float alkalinity, float finalAlkalinity){
-		if(phTarget < pH){
-			return (49.0f/50.0f)*(alkalinity - finalAlkalinity);
-		}else{
-			return 0.0f;
-		}
-	}
+public static float Log10(float x){
+								return (float) Math.log10(x);
+}
+public static float sulfuricAcidCalc(float pH, float phTarget, float alkalinity, float finalAlkalinity){
+								if(phTarget < pH) {
+																return (49.0f/50.0f)*(alkalinity - finalAlkalinity);
+								}else{
+																return 0.0f;
+								}
+}
 }
--- JAVAFILES/LSICCPPCalc.java
+++ JAVAFILES/LSICCPPCalc.java
@@ -1,55 +1,56 @@
-public class LSICCPPCalc{
-	public static void main(String args[]){
+public class LSICCPPCalc {
+public static void main(String args[]){
 
-	float temperature;
-	float tds;
-	float pH;
-	float alkalinity;
-	float phTarget;
-	float finalAlkalinity;
-	float ct;
-	float alpha1;
-	float alpha2;
+								float temperature;
+								float tds;
+								float pH;
+								float alkalinity;
+								float phTarget;
+								float finalAlkalinity;
+								float ct;
+								float alpha1;
+								float alpha2;
 
-	temperature = 10.0f;
-	tds = 57f;
-	pH = 8.5f;
-	alkalinity = 35f;
-	phTarget = 10.3f;
-	
-	//This next section will contain an insane number of variables. I'll do my best to keep them organized.
-	//I'll try and stick to a naming convention. "Calc" means they're from the calc page, Portal means they're from the portal page.
+								temperature = 10.0f;
+								tds = 57f;
+								pH = 8.5f;
+								alkalinity = 35f;
+								phTarget = 10.3f;
 
-	float calcI = tds*0.000025f;
-	float calcE = 60954f/(temperature + 273.15f+116f)-68.937f;
-	float calcA = (float)Math.pow(1820000f*(calcE*(temperature+273.15f)), -1.5f);
-	
-	float calcLogGamma1 = -1*calcA*(float)Math.pow(1, 2*(Math.sqrt(calcI)/(1+Math.sqrt(calcI))-0.3*calcI));
-	float calcLogGamma2 = -1*calcA*(float)Math.pow(2, 2*(Math.sqrt(calcI)/(1+Math.sqrt(calcI))-0.3*calcI));
-	float calcGamma1 = (float)Math.pow(10, calcLogGamma1);
-	float calcGamma2 = (float)Math.pow(10, calcLogGamma2);
+								//This next section will contain an insane number of variables. I'll do my best to keep them organized.
+								//I'll try and stick to a naming convention. "Calc" means they're from the calc page, Portal means they're from the portal page.
 
-	float calcK1 = (float)Math.pow(10, (-1*(356.309-21834.4/(273+temperature)-126.834*
-		Math.log(273+temperature)+0.06092*(273+temperature)+1684915/(273+Math.pow(temperature,2)))));
-	float calcK2 = (float)Math.pow(10, (-1*(107.887-5151.8/(273+temperature)-38.926*
-		Math.log(273+temperature)+0.032528*(273+temperature)+563713.9/(273+Math.pow(temperature,2)))));
-	float calcKw = (float)Math.pow(10, (-1*(-6.088+4471/(273+temperature)+0.01706*(273+temperature))));
-	float calcKso = (float)Math.pow(10,(-1*(171.9065+0.077993*(temperature+273)-2839.319/(temperature+273)-71.595*Math.log10((temperature+273)))));
+								float calcI = tds*0.000025f;
+								float calcE = 60954f/(temperature + 273.15f+116f)-68.937f;
+								float calcA = (float)Math.pow(1820000f*(calcE*(temperature+273.15f)), -1.5f);
 
-	float calcpK1 = (float)-Math.log(calcK1);
-	float calcpK2 = (float)-Math.log(calcK2);
-	float calcpKw = (float)-Math.log(calcKw);
-	float calcpKso = (float)-Math.log(calcKso);
+								float calcLogGamma1 = -1*calcA*(float)Math.pow(1, 2*(Math.sqrt(calcI)/(1+Math.sqrt(calcI))-0.3*calcI));
+								float calcLogGamma2 = -1*calcA*(float)Math.pow(2, 2*(Math.sqrt(calcI)/(1+Math.sqrt(calcI))-0.3*calcI));
+								float calcGamma1 = (float)Math.pow(10, calcLogGamma1);
+								float calcGamma2 = (float)Math.pow(10, calcLogGamma2);
 
-	float calcHpositive = (float)Math.pow(10, pH)/calcGamma1;
-	float calcHOnegative = calcKw/calcHpositive/(float)Math.pow(calcGamma1,2);
+								float calcK1 = (float)Math.pow(10, (-1*(356.309-21834.4/(273+temperature)-126.834*
+																																																Math.log(273+temperature)+0.06092*(273+temperature)+1684915/(273+Math.pow(temperature,2)))));
+								float calcK2 = (float)Math.pow(10, (-1*(107.887-5151.8/(273+temperature)-38.926*
+																																																Math.log(273+temperature)+0.032528*(273+temperature)+563713.9/(273+Math.pow(temperature,2)))));
+								float calcKw = (float)Math.pow(10, (-1*(-6.088+4471/(273+temperature)+0.01706*(273+temperature))));
+								float calcKso = (float)Math.pow(10,(-1*(171.9065+0.077993*(temperature+273)-2839.319/(temperature+273)-71.595*Math.log10((temperature+273)))));
 
-	//float calcCtC03 = (/* h2CO3 */ + /* hCO3 */ + /* CO32 */);
-	float calcH2CO3 = (float)Math.pow(calcGamma1, 2*calcHpositive/calcK1*(alkalinity/50000-calcKw/
-		Math.pow((calcGamma1),2)/calcHpositive+calcHpositive)/(1+2*calcK2/calcGamma2/calcHpositive));
-	float calcHCO3 = (alkalinity/50000-calcKw/(float)Math.pow((calcGamma1), 2)/calcHpositive+calcHpositive)/(1+2*calcK2/calcGamma2/calcHpositive);
-	
+								float calcpK1 = (float)-Math.log(calcK1);
+								float calcpK2 = (float)-Math.log(calcK2);
+								float calcpKw = (float)-Math.log(calcKw);
+								float calcpKso = (float)-Math.log(calcKso);
+
+								float calcHpositive = (float)Math.pow(10, pH)/calcGamma1;
+								float calcHOnegative = calcKw/calcHpositive/(float)Math.pow(calcGamma1,2);
+
+								//float calcCtC03 = (/* h2CO3 */ + /* hCO3 */ + /* CO32 */);
+								float calcH2CO3 = (float)Math.pow(calcGamma1, 2*calcHpositive/calcK1*(alkalinity/50000-calcKw/
+																																																																														Math.pow((calcGamma1),2)/calcHpositive+calcHpositive)/(1+2*calcK2/calcGamma2/calcHpositive));
+								float calcHCO3 = (alkalinity/50000-calcKw/(float)Math.pow((calcGamma1), 2)/calcHpositive+calcHpositive)/(1+2*calcK2/calcGamma2/calcHpositive);
+								float calcCO32negative = calcK2/calcGamma2/calcHpositive*(alkalinity/50000-calcKw/(float)Math.pow(calcGamma1, 2)/calcHpositive+calcHpositive)/(1+2*calcK2/calcGamma2/calcHpositive);
 
 
-	}
+
+}
 }
