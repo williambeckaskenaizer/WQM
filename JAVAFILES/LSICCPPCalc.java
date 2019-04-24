0a6eb2d28f9d7de8a9c92adb3c3162261337bfa2
Will <william.beckaskenaizer@gmail.com>
2019-04-24 00:26:27 -0700

    condensing CCPP calculation into a single method. Also switch to VSC.

--- JAVAFILES/LSICCPPCalc.java
+++ JAVAFILES/LSICCPPCalc.java
@@ -30,7 +30,7 @@ public static void main(String args[]){
 
 //CALCULATED VALUES
 	//double saturationIndex = Math.log(calcLSI());
-	//double CCPP = calcCCPP();
+	double CCPP = calcCCPP();
 	//double aggressiveIndex = calcAI();
 	//double ryznarIndex = calcRI();
 	//double dissolvedOrganicCarbon = calcCtCO3()*12*1000;
@@ -40,12 +40,13 @@ public static void main(String args[]){
 	// System.out.println(CCPPHCO3(12));
 
 	//System.out.println("Saturation Index: " + saturationIndex);
-	//System.out.println("CCPP: " + CCPP);
+	System.out.println("CCPP: " + CCPP);
 	//System.out.println("Aggressive Index: " + aggressiveIndex);
 	//System.out.println("Ryznar Index: " + ryznarIndex);
 	//System.out.println("Dissolved Organic Carbon: " + dissolvedOrganicCarbon);
 	System.out.println("----------------------------------------");
-	//CCPPpH();
+	System.out.println("mister: " + masterCCPP());
+
 // 	CCPPhOld();
 // 	CCPPHCO3();
 // 	CCPPCO3();
@@ -57,109 +58,180 @@ public static void main(String args[]){
 // 	CCPPdCadH();
 // 	CCPPdOHdH();
 // 	CCPPdFdH();
-	CCPPHnew();
+	//CCPPHnew(0);
  }
 
-
-
-//CCPP CALCULATION FORMULAS
-
-public static double CCPPpH(){
-	//FORM: -LOG(V12-47*C18)
-	double x;
-			x = -(Math.log(CCPPHnew() * calcGamma1()));
-	return x;
+public static double masterCCPP(){
+	double ccpppH = 0;
+		double ccpphOld = 0;
+		double ccpphco3;
+		double ccppco3;
+		double ccppCa = 0;
+		double ccppOh;
+		double ccppFh = 0;
+		double ccppdhco3dh;
+		double ccppdco3dh;
+		double ccppdcadh ;
+		double ccppdohdh;
+		double ccppdfdh = 0;
+		double ccppHnew;
+	for(int i = 0; i < 37; i++){
+		System.out.println("loup :3");
+		 ccpppH = -(Math.log(CCPPHnew(ccpphOld, ccppFh, ccppdfdh) * calcGamma1()));
+		 ccpphOld = (Math.pow(10, ccpppH)/calcGamma1());
+		 ccpphco3 = (calcTotalAcidity() + calcKw()/Math.pow(calcGamma1(), 2)/  ccpphOld - ccpphOld/(1 + Math.pow(2, Math.pow(calcGamma1(), 2 )/calcK1()*ccpphOld)));
+		 ccppco3 = (calcK2()/calcGamma2())*( ccpphco3/ccpphOld);
+		 ccppCa = (calcKso()/Math.pow(calcGamma2(), 2)/ccppco3);
+		 ccppOh = (calcKw()/(Math.pow(calcGamma1(),2)/ccpphOld));
+		 ccppFh = (calcTotalAlk() - (2*ccppco3 - ccpphco3 - ccppOh + ccpphOld + 2*ccppCa) );
+		 ccppdhco3dh = ((-calcKw()/(Math.pow(calcGamma1(),2)/Math.pow(ccpphOld,2)-1)
+		*(1+2*Math.pow(calcGamma1(), 2)*ccpphOld/calcK1())-(2*Math.pow(calcGamma1(), 2)
+		/calcK1())*calcTotalAcidity()+calcKw()/Math.pow(calcGamma1(), 2)/ccpphOld - ccpphOld)
+		/(1+2*Math.pow(calcGamma1(), 2)/Math.pow(calcK1() * ccpphOld, 2)));
+		 ccppdco3dh = (calcK2()/calcGamma2()*(ccppdhco3dh*ccpphOld-ccpphco3)/Math.pow(ccpphOld, 2));
+		 ccppdcadh = calcKso()/Math.pow(calcGamma2(), 2)*(-ccppdco3dh)/ccppco3;
+		 ccppdohdh = -calcKw()/Math.pow(calcKw(), 2)/Math.pow(ccpphOld, 2);
+		 ccppdfdh = -2*ccppdco3dh - ccppdhco3dh - ccppdohdh + 1 + 2* ccppdcadh;
+		 ccppHnew = CCPPHnew(ccpphOld, ccppFh, ccppdfdh);
+		 
+	}
+	return ccppCa;
 }
-public static double CCPPhOld(){
-	//FORM: (10^(-J12-49))/C18
-	double x = 0;
-			x = (Math.pow(10, CCPPpH())/calcGamma1());
-	return x;
-}
-
-public static double CCPPHCO3(){
-	//FORM: (C36 + C22 / C18^2 / K12-49 - K12-49)/(1 + 2^C18^2/C20*K12-49) ... oof
-	double x = 0;
-			x = (calcTotalAcidity() + calcKw()/Math.pow(calcGamma1(), 2)/  CCPPhOld() - CCPPhOld()/(1 + Math.pow(2, Math.pow(calcGamma1(), 2 )/calcK1()*CCPPhOld())));
-	return x;
-}
-public static double CCPPCO3(){
-	//FORM: $C$21/$C$19*L12-49/K12-49
-	double x;
-		x = (calcK2()/calcGamma2())*( CCPPHCO3()/CCPPhOld());
-	return x;
-}
-public static double CCPPCa(){
-	//FORM: $C$23/$C$19^2/M13-49
-	double x = 0;
-			x = (calcKso()/Math.pow(calcGamma2(), 2)/CCPPCO3());
-	return x;
-}
-public static double CCPPOH(){
-	//FORM: $C$22/$C$18^2/K12
-	double x = 0;
-			x = (calcKw()/(Math.pow(calcGamma1(),2)/CCPPhOld()));
-	return x;
-}
-public static double CCPPFH(){
-	//FORM: $C$37-2*M12-L12-O12+K12+2*N12
-	double x = 0;
-			x = (calcTotalAlk() - (2*CCPPCO3() - CCPPHCO3() - CCPPOH() + CCPPhOld() + 2*CCPPCa()) );
-	return x;
-}
-public static double CCPPdHCO3dH(){
-	//FORM: ((-$C$22/$C$18^2/K12^2-1)*(1+2*$C$18^2*K12/$C$20)-(2*$C$18^2/$C$20)*($C$36+$C$22/$C$18^2/K12-K12))/(1+2*$C$18^2/$C$20*K12)^2\
-	double x = 0;
-			x = ((-calcKw()/(Math.pow(calcGamma1(),2)/Math.pow(CCPPhOld(),2)-1)
-			*(1+2*Math.pow(calcGamma1(), 2)*CCPPhOld()/calcK1())-(2*Math.pow(calcGamma1(), 2)
-			/calcK1())*calcTotalAcidity()+calcKw()/Math.pow(calcGamma1(), 2)/CCPPhOld() - CCPPhOld())
-			/(1+2*Math.pow(calcGamma1(), 2)/Math.pow(calcK1() * CCPPhOld(), 2)));
-	return x;
-}
-public static double CCPPdCO3dH(){
-	//FORM: $C$21/$C$19*(Q12*K12-L12)/K12^2
-	double x = 0.0;
-			x = (calcK2()/calcGamma2()*(CCPPdHCO3dH()*CCPPhOld()-CCPPHCO3())/Math.pow(CCPPhOld(), 2));
-	return x;
-}
-public static double CCPPdCadH(){
-	//FORM: $C$23/$C$19^2*(-R12)/M12^2
-	double x = 0.0;
-			x = calcKso()/Math.pow(calcGamma2(), 2)*(-CCPPdCO3dH())/CCPPCO3();
-	return x;
-}
-public static double CCPPdOHdH(){
-	double x = 0.0;
-			x = -calcKw()/Math.pow(calcKw(), 2)/Math.pow(CCPPhOld(), 2);
-	return x;
-}
-public static double CCPPdFdH(){
-	//FORM: -2*R12-Q12-T12+1+2*S12
-	double x = 0;
-			x = -2*CCPPdCO3dH() - CCPPdHCO3dH() - CCPPdOHdH() + 1 + 2* CCPPdCadH();
-	return x;
-}
-public static double CCPPHnew(){
+public static double CCPPHnew(double hOld, double fh, double dfdh){
 	//FORM: IF((K12-P12/U12)<0,K12/10,K12-P12/U12)
 	double x;
-		if(CCPPhOld()-CCPPFH()/CCPPdFdH() < 0){
-			x = CCPPhOld()/10;
+		if(hOld-fh/dfdh < 0){
+			x = hOld/10;
 		}else{
-			x = (CCPPhOld()-CCPPFH())/(CCPPdFdH());
+			x = (hOld-fh)/(dfdh);
 		}
 	return x;
 }
-public static double CCPPFlag(){
-//FORM: IF(ABS((J13-J12)/J12)>$J$8,1,0)
-double x = 0;
-	if(Math.abs((CCPPpH() - CCPPpH()/CCPPpH())) > 0.000001){
-		x = 1;
-	}else{
-		x = 0;
-	}
-return x;
-}
 
+
+//CCPP CALCULATION FORMULAS
+//what if i just jammed this all into one giant method.
+//wait. tail recursion, duh...
+//wait, tail recursion is super complicated. not duh...
+//wait maybe it's not.
+
+//CCPPHnew(double hOld, double fh, double dfdh, int index)
+//CCPPdCO3dH(double dHCO3dH, double hOld, double hCO3, int index)
+
+
+// return (CCPPHnew(CCPPhOld(CCPPpH(index-1)), CCPPFH(CCPPCO3(CCPPHCO3(CCPPhOld(CCPPpH(index-1))), CCPPhOld(CCPPpH(index-1))),CCPPHCO3(CCPPhOld(CCPPpH(index-1))),CCPPOH(CCPPhOld(CCPPpH(index-1))),CCPPhOld(CCPPpH(index-1)),
+// CCPPCa(CCPPCO3(CCPPHCO3(CCPPhOld(CCPPpH(index-1))), CCPPhOld(CCPPpH(index-1))))), CCPPdFdH(CCPPdCO3dH(CCPPhOld(CCPPpH(index-1)), CCPPhOld(CCPPpH(index-1)), CCPPhOld(CCPPpH(index-1))),
+//  CCPPdHCO3dH(CCPPhOld(CCPPpH(index-1))), CCPPdOHdH(CCPPhOld(CCPPpH(index-1))), CCPPdCadH(CCPPCO3(CCPPHCO3(CCPPhOld(CCPPpH(index-1))), CCPPhOld(CCPPpH(index-1))),
+// 	CCPPCO3(CCPPHCO3(CCPPhOld(CCPPpH(index-1))), CCPPhOld(CCPPpH(index-1)))))))*calcGamma1();
+
+
+
+// public static double CCPPpH(int index){
+// 	//FORM: -LOG(V12-47*C18)
+// 	double x = 0;
+// if(index > 0){
+// 	System.out.println("loup: " +index);
+// 	return -Math.log(CCPPhnew(index)*calcLogGamma1())
+// 			 // x = x*calcGamma1();
+// 	}else{
+// 		return x;
+// 	}
+// }
+// public static double CCPPhOld(double pH){
+// 	//FORM: (10^(-J12-49))/C18
+// 	double x = 0;
+// 			x = (Math.pow(10, pH)/calcGamma1());
+// 	return x;
+// }
+//
+// public static double CCPPHCO3(double hOld){
+// 	//FORM: (C36 + C22 / C18^2 / K12-49 - K12-49)/(1 + 2^C18^2/C20*K12-49) ... oof
+// 	double x = 0;
+// 	double fHold = hOld;
+// 			x = (calcTotalAcidity() + calcKw()/Math.pow(calcGamma1(), 2)/  hOld - hOld/(1 + Math.pow(2, Math.pow(calcGamma1(), 2 )/calcK1()*hOld)));
+// 	return x;
+// }
+// public static double CCPPCO3(double hCO3, double hOld){
+// 	//FORM: $C$21/$C$19*L12-49/K12-49
+// 	double x;
+// 		x = (calcK2()/calcGamma2())*( hCO3/hOld);
+// 	return x;
+// }
+// public static double CCPPCa(double cO3){
+// 	//FORM: $C$23/$C$19^2/M13-49
+// 	double x = 0;
+// 			x = (calcKso()/Math.pow(calcGamma2(), 2)/cO3);
+// 	return x;
+// }
+// public static double CCPPOH(double hOld){
+// 	//FORM: $C$22/$C$18^2/K12
+// 	double x = 0;
+// 			x = (calcKw()/(Math.pow(calcGamma1(),2)/hOld));
+// 	return x;
+// }
+// public static double CCPPFH(double cO3, double hCO3, double oH, double hOld, double cA){
+// 	//FORM: $C$37-2*M12-L12-O12+K12+2*N12
+// 	double x = 0;
+// 			x = (calcTotalAlk() - (2*cO3 - hCO3 - oH + hOld + 2*cA) );
+// 	return x;
+// }
+// public static double CCPPdHCO3dH(double hOld){
+// 	//FORM: ((-$C$22/$C$18^2/K12^2-1)*(1+2*$C$18^2*K12/$C$20)-(2*$C$18^2/$C$20)*($C$36+$C$22/$C$18^2/K12-K12))/(1+2*$C$18^2/$C$20*K12)^2\
+// 	double x = 0;
+// 			x = ((-calcKw()/(Math.pow(calcGamma1(),2)/Math.pow(hOld,2)-1)
+// 			*(1+2*Math.pow(calcGamma1(), 2)*hOld/calcK1())-(2*Math.pow(calcGamma1(), 2)
+// 			/calcK1())*calcTotalAcidity()+calcKw()/Math.pow(calcGamma1(), 2)/hOld - hOld)
+// 			/(1+2*Math.pow(calcGamma1(), 2)/Math.pow(calcK1() * hOld, 2)));
+// 	return x;
+// }
+// public static double CCPPdCO3dH(double dHCO3dH, double hOld, double hCO3){
+// 	//FORM: $C$21/$C$19*(Q12*K12-L12)/K12^2
+// 	double x = 0.0;
+// 			x = (calcK2()/calcGamma2()*(dHCO3dH*hOld-hCO3)/Math.pow(hOld, 2));
+// 	return x;
+// }
+// public static double CCPPdCadH(double dCO3dH, double cO3){
+// 	//FORM: $C$23/$C$19^2*(-R12)/M12^2
+// 	double x = 0.0;
+// 			x = calcKso()/Math.pow(calcGamma2(), 2)*(-dCO3dH)/cO3;
+// 	return x;
+// }
+// public static double CCPPdOHdH(double hOld){
+// 	double x = 0.0;
+// 			x = -calcKw()/Math.pow(calcKw(), 2)/Math.pow(hOld, 2);
+// 	return x;
+// }
+// public static double CCPPdFdH(double dCO3dH, double dHCO3dH, double dOdH, double cAdH){
+// 	//FORM: -2*R12-Q12-T12+1+2*S12
+// 	double x = 0;
+// 			x = -2*dCO3dH - dHCO3dH - dOdH + 1 + 2* cAdH;
+// 	return x;
+// }
+// public static double CCPPHnew(double hOld, double fh, double dfdh){
+// 	//FORM: IF((K12-P12/U12)<0,K12/10,K12-P12/U12)
+// 	double x;
+// 		if(hOld-fh/dfdh < 0){
+// 			x = hOld/10;
+// 		}else{
+// 			x = (hOld-fh)/(dfdh);
+// 		}
+// 	return x;
+// }
+// public static double CCPPFlag(){
+// //FORM: IF(ABS((J13-J12)/J12)>$J$8,1,0)
+// double x = 0;
+// for(int i = 12; i < 37; i++){
+// 	if(Math.abs((CCPPpH(i+1) - CCPPpH(i)/CCPPpH(i))) > 0.000001){
+// 		x = 1;
+// 	}else{
+// 		x = 0;
+// 	}
+// }
+// return x;
+// }
+
+
+/* END CCPP METHODS */
 
 
 //The guts of the late GetFormula method will go here.
@@ -260,7 +332,7 @@ public static double calcRI(){
 	return 2*calcpHs()-pH;
 }
 public static double calcCCPP(){
-	return calcCalcium-(100000.0 * CCPPCa());
+	return calcCalcium-(100000.0 * masterCCPP());
 }
 
 /*
